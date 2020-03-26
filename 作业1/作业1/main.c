60281fe283c2946ba946f02d4523cf97ebaaf1d8
wangjinjin <874957911@qq.com>
2020-03-26 18:18:56 +0800

    code

---
+++ 作业1/作业1/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  作业1
+//
+//  Created by EDZ on 2020/3/26.
+//  Copyright © 2020 EDZ. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    int a=27,b=35,c;
+    c=a*b;
+    printf("%d\n",c);
+    return 0;
+}
---
+++ 作业2/作业2/main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  作业2
+//
+//  Created by EDZ on 2020/3/26.
+//  Copyright © 2020 EDZ. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    int a=7,b=15,c=23,sum;
+    sum=a+b+c;
+    printf("%d\n",sum);
+    
+    return 0;
+}
